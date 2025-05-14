d060710b00da581495e6d20cbfda9f786f7e5fb4
juktimath003 <juktiamthshreya123@gmail.com>
2025-05-14 11:41:20 +0530

    save it

---
+++ add.java
@@ -0,0 +1,8 @@
+package pgm2;
+public class add {
+    public static void main(String[] args) {        int num1 = 15;
+        int num2 = 25;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
